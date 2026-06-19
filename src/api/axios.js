2264108d0ffd3939f75fbbca0879acd155307d93
NasifNoor <nasifhasannoor@gmail.com.com>
2026-06-19 09:10:20 +0600

    url updated

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.BACKEND_API_URL || "http://localhost:5000/api",
+  baseURL:
+    import.meta.env.BACKEND_API_URL ||
+    "https://limited-edition-sneaker-drop-backend.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
--- src/socket.js
+++ src/socket.js
@@ -1,8 +1,12 @@
 import { io } from "socket.io-client";
 
-const socket = io(import.meta.env.VITE_SOCKET_URL || "http://localhost:5000", {
-  transports: ["websocket"],
-});
+const socket = io(
+  import.meta.env.VITE_SOCKET_URL ||
+    "https://limited-edition-sneaker-drop-backend.onrender.com",
+  {
+    transports: ["websocket"],
+  },
+);
 console.log(
   "🚀 ~ import.meta.env.VITE_SOCKET_URL :",
   import.meta.env.VITE_SOCKET_URL,
